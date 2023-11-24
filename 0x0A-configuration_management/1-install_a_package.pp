dc94eaa5c5407784ed3adf991534eef47701f401
imad bouyfri <imad.bouyfri@outlook.fr>
2023-11-25 00:49:21 +0100

    update install ppt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,8 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'python3-setuptools':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask',
+exec { 'install_flask_2.1.0':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask',
-  require => [
-    Package['python3-pip'],
-    Package['python3-setuptools'],
-  ],
 }
+
