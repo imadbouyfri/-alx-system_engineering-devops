46d963fcf4c7c02bf8fb75e2843d402e0a5407e1
imad bouyfri <imad.bouyfri@outlook.fr>
2023-11-25 00:56:56 +0100

    update install ppt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-exec { 'install_flask_2.1.0':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  path    => ['/usr/bin'],
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
-
