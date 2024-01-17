4ab86938a7273ae3084bfed93410dc1bdb5f47f5
imad bouyfri <imad.bouyfri@outlook.fr>
2024-01-17 03:55:05 +0100

    update 0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
-"""
+""" reddit api function """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API for the number of subscribers of a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers, or 0 if the subreddit is invalid.
     """
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'my_reddit_app/0.0.1'}  # Set a custom User-Agent
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    content = response.json()
+    data = content['data']
+    subscribers = data['subscribers']
+    return subscribers
