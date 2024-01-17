6c84dd6e9e140b94ea0812cc808bb6bbb5436026
imad bouyfri <imad.bouyfri@outlook.fr>
2024-01-17 03:53:21 +0100

    update 0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,16 +4,22 @@
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit), headers=headers, allow_redirects=False)
-    if r.status_code >= 300:
+    """Queries the Reddit API for the number of subscribers of a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my_reddit_app/0.0.1'}  # Set a custom User-Agent
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    json = r.json()
-    data_dict = json.get('data')
-    return(data_dict.get('subscribers'))
