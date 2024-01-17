b1a1cc0e7811a92988093b215720b8477e564683
imad bouyfri <imad.bouyfri@outlook.fr>
2024-01-17 03:52:08 +0100

    update 0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit"""
-
+"""queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers from the Reddit API"""
-    r = requests.get(r'https://www.reddit.com/r/{}/about.json'
-                     .format(subreddit), headers={'User-agent': 'x'},
-                     allow_redirects=False)
-    if r.status_code != 200:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    if r.status_code >= 300:
         return 0
     json = r.json()
-    data = json.get('data')
-    return data.get('subscribers', 0)
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
